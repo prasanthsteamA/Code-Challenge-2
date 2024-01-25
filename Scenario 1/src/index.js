a98aa2a8f041c1bfe0a58b52d3ca5a1ebfa45e45
AnjanaSree22 <anjana@steam-a.com>
2024-01-26 00:14:24 +0530

    completed

--- Scenario 1/src/index.js
+++ Scenario 1/src/index.js
@@ -1,6 +1,9 @@
 
 function increaseNumber(initialValue, finalValue) {
-     //your code here
+  let increased_num;
+  increased_num = finalValue - initialValue;
+  return increased_num; 
+
   }
 
 //Use SpecRunner to check the Test Cases.
--- Scenario 2/src/index.js
+++ Scenario 2/src/index.js
@@ -1,5 +1,10 @@
 function hero(bullets, dragons){
-     //your code here
+  if(bullets/2 >= dragons){
+    return true
+  }
+  else {
+    return false
+  }
   }
 
 //Use SpecRunner to check the Test Cases.
