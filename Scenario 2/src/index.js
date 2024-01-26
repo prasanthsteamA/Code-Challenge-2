16751f2ff2aea840e0f0c8ab5322ce5ace87d2c4
Navinajs <navina@steam-a.com>
2024-01-26 22:49:54 +0530

    first commit

--- Scenario 2/src/index.js
+++ Scenario 2/src/index.js
@@ -1,5 +1,14 @@
 function hero(bullets, dragons){
      //your code here
+     let required=2*dragons;
+     if (required<=bullets){
+      return true;
+     }
+     else {
+      return false;
+     }
+     
   }
+
 
 //Use SpecRunner to check the Test Cases.
--- Scenario 4/src/index.js
+++ Scenario 4/src/index.js
@@ -1,5 +1,23 @@
 function calculateTip(amount, rating) {
     //your code here
+    switch(rating.toLowerCase()){
+      case "terrible":
+        return Math.ceil(0 * amount);
+
+      case "poor":
+        return Math.ceil(0.05 * amount);
+
+      case "good":
+        return Math.ceil(0.1 * amount);
+
+      case "great":
+        return Math.ceil(0.15 * amount);
+
+      case "excellent":
+        return Math.ceil(0.2 * amount);
+    default:
+      return "Rating not recognised";
+    }
   }
 
 //Use SpecRunner to check the Test Cases.
